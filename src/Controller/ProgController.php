f187294ee6160df25caaf36a8c6de92f68c6006c
Evan <94895335+WarLay0@users.noreply.github.com>
2022-11-23 11:47:46 +0100

    :wrench: | Petites corrections avant le début

--- src/Controller/ProgController.php
+++ src/Controller/ProgController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ManifsController extends AbstractController
+class ProgController extends AbstractController
 {
-    #[Route('/manifs', name: 'app_manifs')]
+    #[Route('/prog', name: 'app_prog')]
     public function index(): Response
     {
         return $this->render('manifs/index.html.twig', [
-            'controller_name' => 'ManifsController',
+            'controller_name' => 'ProgController',
         ]);
     }
 }
