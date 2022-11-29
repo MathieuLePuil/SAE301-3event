766eca5f878941e6633934bb4bce95f531dd26d4
MathieuLePuil <mathieu.le-puil@etudiant.univ-reims.fr>
2022-11-29 09:09:04 +0100

    :white_check_mark: | Création de l'Entity Manifestations et ajout dans le dashboard admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,6 +7,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Manifestations;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -25,7 +26,7 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard',);
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Les évènements', 'fas fa-list', Manifestations::class);
     }
 }
---
+++ src/Entity/Manifestations.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ManifestationsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ManifestationsRepository::class)]
+class Manifestations
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    //#[ORM\Column]
+    //private ?int $manif_id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $manif_titre = null;
+
+    #[ORM\Column(length: 1000)]
+    private ?string $manif_description = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $manif_casting = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $manif_genre = null;
+
+    #[ORM\Column]
+    private ?int $manif_prix = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $manif_affiche = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $manif_date = null;
+
+    #[ORM\Column(length: 5)]
+    private ?string $manif_horaire = null;
+
+    #[ORM\Column]
+    private ?int $manif_lieu = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getManifId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setManifId(int $manif_id): self
+    {
+        $this->manif_id = $manif_id;
+
+        return $this;
+    }
+
+    public function getManifTitre(): ?string
+    {
+        return $this->manif_titre;
+    }
+
+    public function setManifTitre(string $manif_titre): self
+    {
+        $this->manif_titre = $manif_titre;
+
+        return $this;
+    }
+
+    public function getManifDescription(): ?string
+    {
+        return $this->manif_description;
+    }
+
+    public function setManifDescription(string $manif_description): self
+    {
+        $this->manif_description = $manif_description;
+
+        return $this;
+    }
+
+    public function getManifCasting(): ?string
+    {
+        return $this->manif_casting;
+    }
+
+    public function setManifCasting(string $manif_casting): self
+    {
+        $this->manif_casting = $manif_casting;
+
+        return $this;
+    }
+
+    public function getManifGenre(): ?string
+    {
+        return $this->manif_genre;
+    }
+
+    public function setManifGenre(string $manif_genre): self
+    {
+        $this->manif_genre = $manif_genre;
+
+        return $this;
+    }
+
+    public function getManifPrix(): ?int
+    {
+        return $this->manif_prix;
+    }
+
+    public function setManifPrix(int $manif_prix): self
+    {
+        $this->manif_prix = $manif_prix;
+
+        return $this;
+    }
+
+    public function getManifAffiche(): ?string
+    {
+        return $this->manif_affiche;
+    }
+
+    public function setManifAffiche(string $manif_affiche): self
+    {
+        $this->manif_affiche = $manif_affiche;
+
+        return $this;
+    }
+
+    public function getManifDate(): ?string
+    {
+        return $this->manif_date;
+    }
+
+    public function setManifDate(string $manif_date): self
+    {
+        $this->manif_date = $manif_date;
+
+        return $this;
+    }
+
+    public function getManifHoraire(): ?string
+    {
+        return $this->manif_horaire;
+    }
+
+    public function setManifHoraire(string $manif_horaire): self
+    {
+        $this->manif_horaire = $manif_horaire;
+
+        return $this;
+    }
+
+    public function getManifLieu(): ?int
+    {
+        return $this->manif_lieu;
+    }
+
+    public function setManifLieu(int $manif_lieu): self
+    {
+        $this->manif_lieu = $manif_lieu;
+
+        return $this;
+    }
+}
