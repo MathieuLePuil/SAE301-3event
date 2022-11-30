40a6a76e8da15f15c334f24b1a6d0d10b3f7c13b
Evan <94895335+WarLay0@users.noreply.github.com>
2022-11-30 09:21:34 +0100

    :white_check_mark: | Récupération des données de la BDD pour afficher dans les salles

--- src/Controller/SallesController.php
+++ src/Controller/SallesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SallesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class SallesController extends AbstractController
 {
     #[Route('/salles', name: 'app_salles')]
-    public function index(): Response
+    public function index(SallesRepository $sallesRepository): Response
     {
         return $this->render('salles/index.html.twig', [
             'controller_name' => 'SallesController',
+            'salles' => $sallesRepository->findAll(),
         ]);
     }
 }
