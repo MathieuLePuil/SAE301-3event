d638f5a31a272a9be524de2d5577e5708b6034f5
Evan <94895335+WarLay0@users.noreply.github.com>
2022-12-01 12:28:53 +0100

    :triangular_flag_on_post: | Sauvegarde 01/12/2022

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\ManifestationsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EventController extends AbstractController
 {
     #[Route('/event', name: 'app_event')]
-    public function index(): Response
+    public function index(ManifestationsRepository $manifestationsRepository, Request $request): Response
     {
+        $id = $request->query->get('id_event');
         return $this->render('event/index.html.twig', [
             'controller_name' => 'EventController',
+            'event' => $manifestationsRepository->findBy(array('id',$id)),
         ]);
     }
 }
